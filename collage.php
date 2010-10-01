54a612373a158a526c9b0f242be072634a916ceb
Patrick Carey <patrickcar@gmail.com>
2010-10-01 09:46:15 -0400

    creation of file

---
+++ collage.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Collage
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Creates an abstract collage of images on your WordPress site.
+Version: 0.1
+Author: Patrick Carey
+Author URI: http://www.immaterial-labour.com
+License: GPL2
+*/
+?>
+
+<?php
+/*  Copyright 2010  Patrick Carey  (email : patrickcar@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
