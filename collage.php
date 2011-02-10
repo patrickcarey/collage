95dd9d90e530ecc20c6885f1c0ad82cbf8b4a1d0
Patrick Carey <patrickcar@gmail.com>
2011-02-09 22:34:43 -0500

    added commenting and documentation, added two display parameters

--- collage.php
+++ collage.php
@@ -24,61 +24,95 @@ License: GPL2
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-?><?php
-
-	function collage($a = -1, $b = 'large') {
+	
+	/**
+	*
+	*collage() 
+	*The function to create and display the image collage
+	* @param int $number , number of images to return, -1 returns all, default is -1
+	* @param string $size , 'thumbnail', 'medium', 'large', default is medium
+	* @param string $layout accepts either 'overlap' or 'block' for how collage displays, default is 'overlap.
+	*
+	**/
+	
+	
+	function collage($number = -1, $size = 'medium', $layout = 'overlap') {
 		
-		
+		#
+		# Set a random width
+		#
 		
 		$rand_width = rand(40,450);
 		
+		
+		#
+		# Arguments for get_posts()
+		#
+		
 		$args = array(
-			
 			
 			'post_type' => 'attachment',
 			
-			//Defines the number of images to return
-			
-			'numberposts' => $a,
+			'numberposts' => $number,
 			
 			'orderby' => rand,
 			
 			);
 			
-		print '<div class="entrycat" style="width:'. $rand_width .'px; float:left; "> 
-
-
-				<div class="img-container" style="float:left;">';
+		
+		
+		print '<div id="collage">';
+		
+		print '<div class="entrycat" style="width:'. $rand_width .'px; float:left; "> <div class="img-container" style="float:left;  ">';
 		
 		$c_posts = get_posts($args);
 		
 		
 		
+		#
+		# If the layout is 'overlap'
+		#
 		
-		foreach($c_posts as $post){
+		if($layout == 'overlap'){
 		
-		print '<div class="img-frame">';
+			foreach($c_posts as $post){
 		
-		$rand_margin = rand(0,100);
-		
-		
-		$collage_image = wp_get_attachment_image_src($post->ID, $b);
-		
-		print '<img src="' . $collage_image[0] . '" alt="collaged images" style="margin:'. $rand_margin .'px;" />';
+				print '<div class="img-frame">';
+	
+				$rand_margin = rand(0,50);
+	
+				$collage_image = wp_get_attachment_image_src($post->ID, $b);
+	
+				print '<img src="' . $collage_image[0] . '" alt="collaged images" style="margin:'. $rand_margin .'px;" /></div>';
 			
-		#print wp_get_attachment_image( $post->ID, $b );
+			}
 		
-			
-		
-		print '</div>';
-			
 		}
 		
-	
-		print '</div>
 		
-				</div>
-		';
+		
+		#
+		# If the layout is 'block'
+		#
+		
+		
+		elseif($layout == 'block'){
+		
+			
+			foreach($c_posts as $post){
+				
+				$collage_image = wp_get_attachment_image_src($post->ID, $size);
+				
+				print '<div class="img-frame-bg" style="background:url('. $collage_image[0]  .')"></div>';
+
+			}
+		
+		
+		}
+		
+		
+	
+		print '</div> </div> </div>';
 		
 		
 		
@@ -93,62 +127,57 @@ License: GPL2
 	}
 	
 	
-	add_action('wp_print_styles', 'collage_stylesheet');
 
    /*
     * Enqueue style-file, if it exists.
     */
 
    function collage_stylesheet() {
-       $myStyleUrl = WP_PLUGIN_URL . '/collage/collage.css';
-       $myStyleFile = WP_PLUGIN_DIR . '/collage/collage.css';
-       if ( file_exists($myStyleFile) ) {
-           wp_register_style('myStyleSheets', $myStyleUrl);
-           wp_enqueue_style( 'myStyleSheets');
-       }
-   }
+       
+	   $myStyleUrl = WP_PLUGIN_URL . '/collage/collage.css';
+       
+	   $myStyleFile = WP_PLUGIN_DIR . '/collage/collage.css';
+       	
+
+		if (file_exists($myStyleFile) ) {
+	        
+			wp_register_style('myStyleSheets', $myStyleUrl);
+	          
+			wp_enqueue_style( 'myStyleSheets');
+	      }
+   
+	}
+	
+	
+/**
+* make_collage()
+* fucntion that returns the collage() function from above, prepared to be a shortcode.
+* 
+* @param $atts takes attributes , which are sent as parameters from the collage() function
+* 
+* @return the collage() as a shortcode
+**/
 
 
 function make_collage($atts){
 	
 	extract(shortcode_atts(array(
+			
 			'number' => '',
+			
 			'size' => '',
+			
+			'type' => '',
+		
 		), $atts)); 
 	
-	return collage($atts['number'], $atts['size']);
+	return collage($atts['number'], $atts['size'], $atts['type']);
 	
 }
 
+add_action('wp_print_styles', 'collage_stylesheet');
 
 add_shortcode('makecollage', 'make_collage');
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
